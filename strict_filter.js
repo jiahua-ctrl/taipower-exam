e375b74e9f0ba7110bdee2bda19f00b231c1325b
jiahua-ctrl <jiahua@chjh.hc.edu.tw>
2026-08-25 23:32:35 +0800

    正式模擬考納入500題進階題庫

--- strict_filter.js
+++ strict_filter.js
@@ -1,9 +1,10 @@
-// 正式模擬考嚴格核對模式：載入高鑑別已核對題並執行品質守門檢查。
+// 正式模擬考嚴格核對模式：載入高鑑別已核對題、500題進階計算題並執行品質守門檢查。
 document.write('<script src="questions_verified_high_discrimination_v1.js"><\/script>');
 document.write('<script src="questions_verified_high_discrimination_v2.js"><\/script>');
 document.write('<script src="questions_verified_high_discrimination_v3.js"><\/script>');
 document.write('<script src="questions_verified_high_discrimination_v4.js"><\/script>');
 document.write('<script src="questions_verified_high_discrimination_v5.js"><\/script>');
+document.write('<script src="questions_advanced_v2_500.js"><\/script>');
 document.write('<script src="question_bank_guard.js"><\/script>');
 window.LOCAL_QUESTIONS = typeof window.auditVerifiedQuestionBank === "function"
   ? window.auditVerifiedQuestionBank(window.LOCAL_QUESTIONS)
